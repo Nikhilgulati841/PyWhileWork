ca58548b7fb55b57d85f5648ce82e5e9a8d5f3d9
Nikhil Gulati <66572138+Nikhilgulati841@users.noreply.github.com>
2024-11-15 21:36:17 +0530

    Create date-time.py

---
+++ date-time.py
@@ -0,0 +1,30 @@
+from datetime import datetime, date, time, timedelta
+import pytz
+#Normal Timezone
+a=datetime.now()
+print(a)
+print("\n")
+#Chnaging timezone
+set_timezone=pytz.timezone("Asia/Kolkata")
+timeframe=datetime.now(set_timezone)
+#Setting the format of the view point of the date you want
+b=timeframe.strftime("Weekday: %A\nTime: %I:%M:%S %p\nDate: %d-%b-%Y")
+print(b)
+print("\n")
+add=int(input("How many days to today: "))
+print(f"\nAdding...!!| {add} days added to today:\n")
+#adding days to the timezone changed recently (timeframe->correct : b->incorrect)
+c=timeframe+timedelta(days=add)
+print(c.strftime("Weekday: %A\nTime: %I:%M:%S %p\nDate: %d-%b-%Y"))
+
+#Now add days to the date given by the user (strptime)
+user_input_date=input("\nEnter the date you wish to get added | Format[day(XX)-month(XX)-year(XXXX)]: ")
+
+user_date=datetime.strptime(user_input_date,"%d-%m-%Y")
+print("\n")
+print(f"Adding {add} days...!! to the user inputted date: {user_input_date}\n")
+d=user_date+timedelta(days=add)
+print(d.strftime("Weekday: %A\nTime: %I:%M:%S %p\nDate: %d-%b-%Y"))
+
+#How to get analyze the timezone oif the given time according to the current printed timings
+
