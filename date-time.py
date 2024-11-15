057041277d3f217b1a431f90dad6d747985f2ffb
Nikhil Gulati <66572138+Nikhilgulati841@users.noreply.github.com>
2024-11-15 21:55:18 +0530

    Update date-time.py

--- date-time.py
+++ date-time.py
@@ -3,6 +3,7 @@ import pytz
 #Normal Timezone
 a=datetime.now()
 print(a)
+print(f"TimeZone Name: {a.tzinfo}")
 print("\n")
 #Chnaging timezone
 set_timezone=pytz.timezone("Asia/Kolkata")
@@ -10,6 +11,10 @@ timeframe=datetime.now(set_timezone)
 #Setting the format of the view point of the date you want
 b=timeframe.strftime("Weekday: %A\nTime: %I:%M:%S %p\nDate: %d-%b-%Y")
 print(b)
+#name of the timezones
+print(f"TimeZone Name: {timeframe.tzname()}")
+print(f"TimeZone: {timeframe.tzinfo}")
+
 print("\n")
 add=int(input("How many days to today: "))
 print(f"\nAdding...!!| {add} days added to today:\n")
@@ -27,4 +32,5 @@ d=user_date+timedelta(days=add)
 print(d.strftime("Weekday: %A\nTime: %I:%M:%S %p\nDate: %d-%b-%Y"))
 
 #How to get analyze the timezone oif the given time according to the current printed timings
+
 
