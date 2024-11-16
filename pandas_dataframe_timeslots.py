84dda750ba4d24bcd061bda56dc9def42c08ca96
Nikhil Gulati <66572138+Nikhilgulati841@users.noreply.github.com>
2024-11-16 22:12:23 +0530

    Update pandas_dataframe_timeslots.py

--- pandas_dataframe_timeslots.py
+++ pandas_dataframe_timeslots.py
@@ -7,20 +7,25 @@
 import pandas as pd
 from datetime import datetime 
 
-a=datetime.now()
-print(a)
+# a=datetime.now()
+# print(a)
 
+        
 
-data_dict={:["Reached Room, ready for studying",                    "Start Studying","Study till here","REVISION",
-                        "Exercise, bath & Sleep"],
-        "REMINDERS   ":["a","b","c","d","e"],
-        "T Slots":["@4:10 Am","@4:30 Am","@10:30 Am","@11:30 Am","@12:30 Pm"]}
+data_dict={"T Slots":["4:10 Am","@4:30 Am","@10:00 Am","@11:00 Am","@12:00 Pm","5:00 Pm","5:40 Pm","6:00 Pm","","7:45 Pm","10:45 Pm","1:30 Am","3:45 Am"],
+
+"DEFINED TASKS          ":["Reached Room, ready for studying",                    "Start Studying","Study till here","Revision Time",
+                        "Exercise Buddy, bath & Sleep","Sleep Till here","Ready for Office","Office Ride Book",""],
+                        
+        "REMINDER   ":["a","b","c","d","e"],"HELP":["a","b","c","d","e"]}
 
 data=pd.DataFrame(data_dict)
 # data["TASKS"].str.pad(width=4,fillchar="T",side="both")
 data=data.set_index("T Slots")
 print(data)
 
+# # b=datetime.now()
+# # print(b)
 
 """--------------------------------------------------------------------------
 --------------------------------------------------------------------------
