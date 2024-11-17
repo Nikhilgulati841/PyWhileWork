bd2de8b2bfa9998f4660785c31027dab0c923a52
Nikhil Gulati <66572138+Nikhilgulati841@users.noreply.github.com>
2024-11-17 05:56:22 +0530

    Update date-time.py

--- date-time.py
+++ date-time.py
@@ -2,8 +2,9 @@ from datetime import datetime, date, time, timedelta
 import pytz       #BEGGINING OF 2025, I WILL APPLY FOR PYTHON DEVELOPER JOBS
 #Normal Timezone
 a=datetime.now()
-print(a)
+print("\n",a)
 print(f"TimeZone Name: {a.tzinfo}")
+print(f"TimeZone Name: {a.tzname()}")
 print("\n")
 #Chnaging timezone
 set_timezone=pytz.timezone("Asia/Kolkata")
@@ -22,15 +23,17 @@ print(f"\nAdding...!!| {add} days added to today:\n")
 c=timeframe+timedelta(days=add)
 print(c.strftime("Weekday: %A\nTime: %I:%M:%S %p\nDate: %d-%b-%Y"))
 
-#Now add days to the date given by the user (strptime)
-user_input_date=input("\nEnter the date you wish to get added | Format[day(XX)-month(XX)-year(XXXX)]: ")
+ask=input("\nDo you wish to add these days to any other date or not: ")
+if ask=="Yes" or ask=="yes":
+    #Now add days to the date given by the user (strptime)
+    user_input_date=input("\nEnter the date you wish to get added | Format[day(XX)-month(XX)-year(XXXX)]: ")
 
-user_date=datetime.strptime(user_input_date,"%d-%m-%Y")
-print("\n")
-print(f"Adding {add} days...!! to this DATE: {user_input_date}\n")
-d=user_date+timedelta(days=add)
-print(d.strftime("Weekday: %A\nTime: %I:%M:%S %p\nDate: %d-%b-%Y"))
+    user_date=datetime.strptime(user_input_date,"%d-%m-%Y")
+    print("\n")
+    print(f"Adding {add} days...!! to this DATE: {user_input_date}\n")
+    d=user_date+timedelta(days=add)
+    print(d.strftime("Weekday: %A\nTime: %I:%M:%S %p\nDate: %d-%b-%Y"))
 
-#How to get analyze the timezone oif the given time according to the current printed timings
-
-
+    #How to get analyze the timezone of the given time according to the current printed timings
+else:
+    print("\n***Exited***\n")
